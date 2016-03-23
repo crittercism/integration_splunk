b14ff2eda5bddea5f69cd05cef772ecc59138dff
dovekie <dovekie@gmail.com>
2016-03-23 12:25:04 -0700

    Mock the API call and test summary methods

--- bin/test.py
+++ bin/test.py
@@ -1,29 +1,79 @@
 import unittest
 import mock
+import datetime
+
 import critterget
 
 class TestSplunk(unittest.TestCase):
     def setUp(self):
-        self.debug = 1
+        critterget.debug = 1
+        critterget.access_token = "bogustoken"
+
+        get_patcher = mock.patch.object(critterget.requests, 'get')
+
+        self.mock_get = get_patcher.start()
+        self.addCleanup(get_patcher.stop)
 
     def tearDown(self):
         pass
 
-    def testGetCredentials(self):
+    # This method was lifted from cypythonlib/soaclients/tests/unit/test_ams_wrapper.py
+    @staticmethod
+    def _response_with_json_data(status_code, json_data):
+        response = mock.Mock()
+        response.status_code = status_code
+        response.json.return_value = json_data
+        return response
+
+    def test_get_credentials(self):
         credentials = critterget.getCredentials('session_key')
         self.assertEqual(credentials, 'boguspassword')
 
-    @mock.patch('critterget.apicall.request')
-    def testGetOk(self, mock_get):
-        mock_response = mock.Mock()
-        expected_dict = {'appID': {'bogusresponse': 'bogusdata'}}
+    def test_apicall(self):
+        self.mock_get.side_effect = [self._response_with_json_data(200, {'bogusappID': {'bogusresponse': 'bogusdata'}})]
+        test_response = critterget.apicall('endpoint', 'attribute string')
+        self.assertEqual(test_response, {'bogusappID': {'bogusresponse': 'bogusdata'}})
 
-        mock_response.json.return_value = expected_dict
+    def test_getAppSummary(self):
+        self.mock_get.side_effect = [self._response_with_json_data(200,
+                                                                   {'bogusappID':
+                                                                        {'appName': 'bogusApp',
+                                                                         'appType': 'bogusType',
+                                                                         'crashPercent': 'bogusCrash',
+                                                                         'dau': 'bogusDAU',
+                                                                         'latency': 'bogusLatency',
+                                                                         'latestAppStoreReleaseDate': 'bogusDate',
+                                                                         'latestVersionString': 'bogusVersion',
+                                                                         'linkToAppStore': 'bogusLink',
+                                                                         'iconURL': 'bogusURL',
+                                                                         'mau': 'bogusMAU',
+                                                                         'rating': 'bogusRating',
+                                                                         'role': 'bogusRole'}
+                                                                    }
+                                                                   )
+                                     ]
+        apps = critterget.getAppSummary()
+        self.assertEqual(apps.keys()[0], 'bogusappID')
+        self.assertEqual(apps[apps.keys()[0]], 'bogusApp')
 
-        mock_get.return_value = mock_response
+    def test_scopetime(self):
+        test_time = critterget.scopetime()
+        interval_time = datetime.datetime.utcnow() - datetime.timedelta(minutes=10)
+        self.assertLessEqual(test_time, interval_time.isoformat())
 
-        response_dict = critterget.apicall('uri', 'attribs')
-
-        mock_get.assert_called_once_with('uri')
-        self.assertEqual(1, mock_response.json.call_count)
-        self.assertEqual(response_dict, expected_dict)
+    def test_getCrashSummary(self):
+        self.mock_get.side_effect = [self._response_with_json_data(200,[
+                                                                         {'hash': 'bogusHash',
+                                                                          'lastOccurred': 'bogusLast',
+                                                                          'sessionCount': 'bogusCount',
+                                                                          'uniqueSessionCount': 'bogusUniqueCount',
+                                                                          'reason': 'bogusReason',
+                                                                          'status': 'bogusStatus',
+                                                                          'displayReason': 'bogusDisplayReason',
+                                                                          'name': 'bogusName'}
+                                                                        ]
+                                                                   )
+                                     ]
+        crashes = critterget.getCrashSummary('bogusappID', 'bogusName')
+        print "CRASHES", crashes
+        self.assertEqual(crashes, {'bogusHash': 'bogusName'})
